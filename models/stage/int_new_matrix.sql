72eb357711292b985b26c6d77f8c153b63b126c8
Pouria Modaresi <pouria@ifiveo.com>
2022-02-11 23:38:18 +0000

    Lateral pad

--- models/stage/int_new_matrix.sql
+++ models/stage/int_new_matrix.sql
@@ -5,7 +5,26 @@ with cte as (SELECT
              station_id,
              assignment_id,
              part_number
-            from {{ ref('new_matrix_source') }})
+            from {{ ref('new_matrix_source') }}
+            WHERE  part_number IN ('INDPART2801',
+                                     'INDPART2802'
+                                     'ST Lateral Pad',
+                                     'INDPART2803',
+                                     'ST-UTP544056100-Q',
+                                     'Pad ST-UTP548056100-Q',
+                                     'Pad ST-UTP54D056100-Q',
+                                     'INDPART2804',
+                                     'INDPART2805',
+                                     'INDPART2854',
+                                     'POS140428',
+                                     'POS140429',
+                                     'ST-UTTP5-Q',
+                                     'ST-UTTP7-Q',
+                                     'INDPART2677',
+                                     'INDPART2896',
+                                     'ST-TWBLT-QL-L',
+                                     'ST-TWBLT-QL-R',
+                                     'ST-ULS4042000-Q' ))
 select *,
       convert_timezone('UTC','America/New_York',utc_timestamps) AS local_Timestamp
 from cte
