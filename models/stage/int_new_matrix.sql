024809dfda3f48901f61331e693b4e3d23f1f902
Pouria Modaresi <pouria@ifiveo.com>
2022-02-17 16:12:09 +0000

    testing

--- models/stage/int_new_matrix.sql
+++ models/stage/int_new_matrix.sql
@@ -4,28 +4,10 @@ with cte as (SELECT
              station_id,
              assignment_id,
              part_number
-            from {{ ref('new_matrix_source') }}
-            WHERE  part_number IN ('INDPART2801',
-                                     'INDPART2802'
-                                     'ST Lateral Pad',
-                                     'INDPART2803',
-                                     'ST-UTP544056100-Q',
-                                     'Pad ST-UTP548056100-Q',
-                                     'Pad ST-UTP54D056100-Q',
-                                     'INDPART2804',
-                                     'INDPART2805',
-                                     'INDPART2854',
-                                     'POS140428',
-                                     'POS140429',
-                                     'ST-UTTP5-Q',
-                                     'ST-UTTP7-Q',
-                                     'INDPART2677',
-                                     'INDPART2896',
-                                     'ST-TWBLT-QL-L',
-                                     'ST-TWBLT-QL-R',
-                                     'ST-ULS4042000-Q' ))
-select *,
+            from {{ ref('new_matrix_source') }})
+            
+ select *,
       convert_timezone('UTC','America/New_York',scanned_at) AS local_Timestamp,
-      date_trunc('minute',dateadd(second,'80',local_Timestamp)) as added_time
+      date_trunc('minute',dateadd(second,'60',local_Timestamp)) as added_time
 
 from cte
---
+++ models/stage/int_test_join.sql
@@ -0,0 +1,18 @@
+----joned table--
+{{ config(
+    materialized='ephemeral'
+)}}
+with cte as (select m.station_id,
+                    m.scanned_at,
+                    m.local_timestamp,
+                    m.assignment_id,
+                    m.part_number,
+                    q.part_detected 
+                    from {{ ref('int_test_new_matrix') }} m
+     left JOIN  {{ ref('int_test_new_qc') }} q
+ON 
+    m.station_id =q.station_detected and
+    m.added_time=q.detected_timestamp
+    and m.part_number=q.part_detected
+order by m.added_time desc)
+select * from cte  order by local_timestamp desc 
